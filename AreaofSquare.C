44beaa6082125babab044e8e4ec98d034ba1c445
Ayushpanchal26 <113408381+Ayushpanchal26@users.noreply.github.com>
2022-10-03 21:50:49 +0530

    Add files via upload

---
+++ AreaofSquare.C
@@ -0,0 +1,24 @@
+// Prpgram to calculate the area of SQUARE:
+
+#include<stdio.h>
+
+main()
+
+{
+   
+       int side;
+  
+       printf("Enter side");
+
+       scanf("%d", &side);
+  
+       printf("%d", side*side);
+  
+       scanf("%d", &side);
+   
+       if(side!=-1){main();}
+ 
+
+return 0;
+
+}
