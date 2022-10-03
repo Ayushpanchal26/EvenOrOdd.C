0ca42aa19df967571b1c7bd6728c48a35f4b3cd4
Ayushpanchal26 <113408381+Ayushpanchal26@users.noreply.github.com>
2022-10-03 21:39:34 +0530

    Add files via upload

---
+++ EvenAndOdd.c
@@ -0,0 +1,23 @@
+// Program to check if a number is ODD or EVEN
+.
+#include<math.h>
+
+//  EVEN --> 1
+
+//  ODD -->  0
+
+int main(){
+
+        
+              int x;
+   
+              printf("Enter a number");
+ 
+              scanf("%d", &x);
+   
+              printf("%d", x%2 == 0);
+
+
+return 0;
+
+}
